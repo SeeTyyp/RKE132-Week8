6718b6d3d8ae44e160b5ad9f8780bcbd32e84024
SeeTyyp <puusepp.imre@gmail.com>
2023-04-23 22:53:26 +0300

    Week8

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,93 @@
+﻿
+string folderpath = @"C:\Users\Kasutaja\OneDrive\Documents\DataProge\";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderpath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderpath, villainFile));
+
+//string[] heroes = { "Harry Potter", "Luke Skywalker", "Lara Croft", "Scooby-Doo" };
+//string[] villains = { "Voldemort", "Darth Vader", "Dracula", "Joker", "Sauron" };
+string[] weapon = { "magic wand", "plastic fork", "banana", "wooden sword", "lego brick" };
+
+//Random rnd = new Random();
+//int randomIndex = rnd.Next(0, heroes.Length);
+//string hero = heroes[randomIndex];
+//Console.WriteLine($"Today {hero} saves the day!");
+//randomIndex = rnd.Next(0, villains.Length);
+//string villain = villains[randomIndex];
+//Console.WriteLine($"Today {villain} tries to take over the world!");
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+int heroHP = GetCharacterHP(hero);
+int heroStrikeStrengh = heroHP;
+Console.WriteLine($"Today {hero} ({heroHP} HP) with {heroWeapon} saves the day!");
+
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+int villainHP = GetCharacterHP(villain);
+int villainStrikeStrengh = villainHP;
+Console.WriteLine($"Today {villain} ({villainHP} HP) with {villainWeapon} tries to take over the world!");
+
+while(heroHP > 0 && villainHP > 0)
+{
+    heroHP = heroHP - Hit(villain, villainStrikeStrengh);
+    villainHP = villainHP - Hit(hero, heroStrikeStrengh);
+}
+
+Console.WriteLine($"Hero {hero} HP: {heroHP}");
+Console.WriteLine($"Villain {villain} HP: {villainHP}");
+
+if(heroHP > 0)
+{
+    Console.WriteLine($"{hero} saves the day!");
+}
+else if (villainHP > 0)
+{
+    Console.WriteLine($"{villain} took over the world! :(");
+}
+else
+{
+    Console.WriteLine("Draw!");
+}
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray; 
+}
+
+static int GetCharacterHP(string characterName)
+{
+    if(characterName.Length < 10)
+    {
+        return 10;
+    }
+    else
+    {
+        return characterName.Length;
+    }
+}
+
+static int Hit(string characterName,int characterHP)
+{
+    Random rnd = new Random();
+    int strike = rnd.Next(0, characterHP);
+
+    if(strike == 0)
+    {
+        Console.WriteLine($"{characterName} missed the target!");
+    }
+    else if(strike == characterHP -1)
+    {
+        Console.WriteLine($"{characterHP} made a critical hit!");
+    }
+    else
+    {
+        Console.WriteLine($"{characterName} hit {strike}!");
+    }
+    return strike;
+}
